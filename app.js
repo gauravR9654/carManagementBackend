4952dc37982dd6b35c6566e3f0d37277d6ab9f37
gauravR9654 <gaurav@katchintech.com>
2024-11-15 12:02:22 +0530

    backnd

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ app.use('/cars', searchQuery);
 
 // Define the server port
 const PORT = process.env.PORT || 4000;
+app.get('/', (req, res)=>{
+  res.send("hello")
+})
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
